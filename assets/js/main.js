7d03fd84c3fe8f2ee2324dde540e30979cb1306b
Jose Navarrete Del Rosario <josenavarretedr@gmail.com>
2022-02-27 23:15:50 -0500

    inicial commit -falta hacer el footer

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+let slider = new Splide( '#image-slider',{
+  type: 'loop',
+  autoplay: true,
+  rewind: true,
+  pauseOnHover: true,
+  pauseOnFocus: false,
+})
+
+slider.mount()
+
+console.log('Hola')
