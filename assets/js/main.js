855c90f7e08e4f2a8f7f0caeacae0d0288ca2c3e
Jose Navarrete Del Rosario <josenavarretedr@gmail.com>
2022-03-17 02:34:40 -0500

    1.0.0 all css is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,14 @@
 
-let slider = new Splide( '#image-slider',{
+let slider = new Splide('#image-slider', {
   type: 'loop',
   autoplay: true,
   rewind: true,
   pauseOnHover: true,
   pauseOnFocus: false,
+  autoWidth: false,
+  // padding: { left: 10, right: 20 }
+  // padding: { left: '1rem', right: '2rem' }
+  // cover: false,
 })
 
 slider.mount()
